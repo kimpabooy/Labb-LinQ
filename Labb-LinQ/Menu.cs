690756468bfe28445317cadeba781e0c3f92456c
Kim Andersson <Kimpabooy@live.com>
2025-04-12 16:32:30 +0200

    Made some finishing touches to the menu

--- Labb-LinQ/Menu.cs
+++ Labb-LinQ/Menu.cs
@@ -11,64 +11,66 @@ namespace Labb_LinQ
             while (true)
             {
                 Console.Clear();
-
-                Console.WriteLine("\n << MENY >>\n");
+                Console.WriteLine("################################################");
+                Console.WriteLine("#      << E-Commerce Database MENY >>          #");
+                Console.WriteLine("################################################\n");
                 Console.WriteLine("1. Visa alla produkter i kategorin Elektronik");
                 Console.WriteLine("2. Visa leverantörer med lågt lager");
                 Console.WriteLine("3. Visa totala ordervärdet för den senaste månaden");
                 Console.WriteLine("4. Visa de tre mest sålda produkterna");
                 Console.WriteLine("5. Lista alla produkter i varje kategori");
                 Console.WriteLine("6. Visa alla ordrar med information om kund och produkt");
-                Console.WriteLine("7. Avsluta programmet\n");
-                Console.Write("\nAnge ditt val (1-7): ");
+                Console.WriteLine("\n0. Avsluta programmet\n");
+                Console.Write("\nAnge ditt val (1-6): ");
 
-                // Make sure the user input is valid
-                string? choice = Console.ReadLine();
-                if (!int.TryParse(choice, out int inputInt) || inputInt < 1 || inputInt > 7)
+                // Make sure the user's input is valid.
+                string? input = Console.ReadLine();
+                if (!int.TryParse(input, out int inputInt) || inputInt < 0 || inputInt > 6)
                 {
                     Console.WriteLine("Ogiltigt val. Försök igen.");
+                    Console.ReadKey();
                     continue;
                 }
 
-                switch (choice)
+                switch (inputInt)
                 {
-                    case "1":
-                        // Show all products in the Electronics category
+                    case 1:
+                        // Show all products in the Electronics category.
                         PleaseWait();
                         linqQueries.ShowElectronicsProducts(context);
                         PressAnyKey();
                         break;
-                    case "2":
+                    case 2:
                         // Show suppliers with low stock.
                         PleaseWait();
                         linqQueries.ShowSupplierWithLowAmmount(context);
                         PressAnyKey();
                         break;
-                    case "3":
+                    case 3:
                         // Show total order value for the last month.
                         PleaseWait();
                         linqQueries.ShowTotalOrderValueLastMonth(context);
                         PressAnyKey();
                         break;
-                    case "4":
+                    case 4:
                         // Show top three sold products.
                         PleaseWait();
                         linqQueries.TopThreeProductSold(context);
                         PressAnyKey();
                         break;
-                    case "5":
+                    case 5:
                         // Show list of all products in a specific category.
                         PleaseWait();
                         linqQueries.ListAllProductInCategory(context);
                         PressAnyKey();
                         break;
-                    case "6":
+                    case 6:
                         // Show all orders with customer and product info.
                         PleaseWait();
                         linqQueries.OrdersWithInfo(context);
                         PressAnyKey();
                         break;
-                    case "7":
+                    case 0:
                         // Exit the program.
                         Console.Clear();
                         Console.WriteLine("\nAvslutar programmet...");
